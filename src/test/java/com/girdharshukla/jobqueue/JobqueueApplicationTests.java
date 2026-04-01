db8d9ec7ddb244270c798f244a52cc759b392069
Girdhar Shukla <girdharshukla26@gmail.com>
2026-04-01 23:27:13 +0530

    init

---
+++ src/test/java/com/girdharshukla/jobqueue/JobqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girdharshukla.jobqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
